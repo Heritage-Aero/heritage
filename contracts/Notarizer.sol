8eb694ab951ab54d90b6c38e0a4645352bff9350
hayek <AlexSherbuck@gmail.com>
2018-07-31 00:39:53 -0700

    split badge logic out, badge inherits donation. Each is their own erc-721

--- contracts/Notarizer.sol
+++ contracts/Notarizer.sol
@@ -3,19 +3,10 @@ pragma solidity ^0.4.24;
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'zeppelin-solidity/contracts/ownership/NoOwner.sol';
 import './Managed.sol';
-import './Donation.sol';
+import './Badge.sol';
 
 // Needs gas opti
-contract Notarizer is Ownable, Managed, Donation, NoOwner {
-
-Badge[] public badges;
-uint256 public totalRaised;
-
-  struct Badge {
-    uint64 donationId;
-    uint128 amount;
-    address donor;
-  }
+contract Notarizer is Ownable, Managed, Badge, NoOwner {
 
   constructor() public payable {
     require(msg.value == 0);
@@ -48,29 +39,5 @@ uint256 public totalRaised;
     }
 
     return _makeDonation(_donationId, uint128(msg.value), msg.sender);
-  }
-
-  function _makeDonation(uint64 _donationId, uint128 _amount, address _donor)
-    internal
-    returns (uint256)
-  {
-    Badge memory _badge = Badge({
-      donationId: _donationId,
-      amount: _amount,
-      donor: _donor
-    });
-
-    uint256 newBadgeId = badges.push(_badge);
-
-    _mint(msg.sender, newBadgeId);
-
-    donations[_donationId].raised += _amount;
-    donations[_donationId].beneficiary.transfer(_amount);
-    totalRaised += _amount;
-    return newBadgeId;
-  }
-
-  function totalBadges() external view returns (uint256 _totalBadges) {
-    _totalBadges = badges.length;
   }
 }
